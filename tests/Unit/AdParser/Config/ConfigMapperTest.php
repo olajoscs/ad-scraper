169a48e7582852260927b902cd486865d8b50319
Csaba Olajos <olajos.csaba.88@gmail.com>
2023-10-23 12:17:03 +0200

    Improve config mapping test

--- tests/Unit/AdParser/Config/ConfigMapperTest.php
+++ tests/Unit/AdParser/Config/ConfigMapperTest.php
@@ -16,10 +16,69 @@ class ConfigMapperTest extends TestCase
     private $configMapper;
 
 
+    public function testPropertyValues(): void
+    {
+        $key1 = 'test1';
+        $name1 = 'test1';
+        $url1 = 'test1';
+        $recipients1 = ['test1', 'test1'];
+        $filters1 = ['test1', 'test1'];
+        $exclusions1 = ['test1', 'test1'];
+
+        $key2 = 'test2';
+        $name2 = 'test2';
+        $url2 = 'test2';
+        $recipients2 = ['test2', 'test2'];
+        $filters2 = ['test2', 'test2'];
+        $exclusions2 = ['test2', 'test2'];
+
+        $config = [
+            'config' => [
+                [
+                    'key' => $key1,
+                    'name' => $name1,
+                    'url' => $url1,
+                    'recipients' => $recipients1,
+                    'filters' => $filters1,
+                    'exclusions' => $exclusions1,
+                ],
+                [
+                    'key' => $key2,
+                    'name' => $name2,
+                    'url' => $url2,
+                    'recipients' => $recipients2,
+                    'filters' => $filters2,
+                    'exclusions' => $exclusions2,
+                ],
+            ],
+        ];
+
+        $parsedConfig = $this->configMapper->map($config);
+
+        $this->assertCount(2, $parsedConfig->siteConfigs);
+        $siteConfig1 = $parsedConfig->siteConfigs[0];
+        $siteConfig2 = $parsedConfig->siteConfigs[1];
+
+        $this->assertSame($key1, $siteConfig1->key);
+        $this->assertSame($name1, $siteConfig1->name);
+        $this->assertSame($url1, $siteConfig1->siteUrl);
+        $this->assertSame($recipients1, $siteConfig1->recipients);
+        $this->assertSame($filters1, $siteConfig1->filters);
+        $this->assertSame($exclusions1, $siteConfig1->exclusionFilters);
+
+        $this->assertSame($key2, $siteConfig2->key);
+        $this->assertSame($name2, $siteConfig2->name);
+        $this->assertSame($url2, $siteConfig2->siteUrl);
+        $this->assertSame($recipients2, $siteConfig2->recipients);
+        $this->assertSame($filters2, $siteConfig2->filters);
+        $this->assertSame($exclusions2, $siteConfig2->exclusionFilters);
+    }
+
+
     /**
      * @dataProvider provideTestData
      */
-    public function test(array $rawConfig, ?string $expectedExceptionType): void
+    public function testMissingProperties(array $rawConfig, ?string $expectedExceptionType): void
     {
         if ($expectedExceptionType) {
             $this->expectException($expectedExceptionType);
